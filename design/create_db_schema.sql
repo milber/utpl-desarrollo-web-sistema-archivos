e97cb0d7129bcc46e7811ee3b2bf41085e834fcc
MACB <milber.ch@gmail.com>
2026-06-18 17:48:17 -0500

    update script for create db

--- design/create_db_schema.sql
+++ design/create_db_schema.sql
@@ -5,14 +5,14 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema macb_ape
+-- Schema macb_archivos
 -- -----------------------------------------------------
 DROP SCHEMA IF EXISTS `macb_archivos` ;
 
 -- -----------------------------------------------------
--- Schema macb_ape
+-- Schema macb_archivos
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `macb_ape` DEFAULT CHARACTER SET utf8 ;
+CREATE SCHEMA IF NOT EXISTS `macb_archivos` DEFAULT CHARACTER SET utf8 ;
 USE `macb_archivos` ;
 
 -- -----------------------------------------------------
@@ -23,24 +23,34 @@ CREATE TABLE IF NOT EXISTS `macb_archivos`.`usuarios` (
   `cedula` VARCHAR(10) NOT NULL,
   `nombre` VARCHAR(100) NOT NULL,
   `correo` VARCHAR(100) NOT NULL,
-  `clave_segura` VARCHAR(255) NOT NULL,
   `fecha_registro` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `fecha_actualizacion` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `clave_segura` VARCHAR(255) NOT NULL,
   UNIQUE INDEX `cedula_UNIQUE` (`cedula` ASC) VISIBLE,
   UNIQUE INDEX `correo_UNIQUE` (`correo` ASC) VISIBLE,
   PRIMARY KEY (`id_usuario`))
 ENGINE = InnoDB;
 
+
 -- -----------------------------------------------------
--- Table `macb_archivos`.`formulario_contacto`
+-- Table `macb_archivos`.`archivos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `macb_ape`.`formulario_contacto` (
+CREATE TABLE IF NOT EXISTS `macb_archivos`.`archivos` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `correo` VARCHAR(45) NOT NULL,
-  `nombre` VARCHAR(100) NOT NULL,
-  `mensaje` LONGTEXT NOT NULL,
-  `fecha_registro` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`))
+  `nombre_original` VARCHAR(50) NOT NULL,
+  `nombre_archivo` VARCHAR(100) NULL,
+  `tipo` VARCHAR(3) NOT NULL,
+  `tamanio` INT NOT NULL,
+  `carpeta` VARCHAR(45) NOT NULL,
+  `fecha_subida` VARCHAR(45) NOT NULL DEFAULT 'CURRENT_TIMESTAMP',
+  `usuarios_id_usuario` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_archivos_usuarios_idx` (`usuarios_id_usuario` ASC) VISIBLE,
+  CONSTRAINT `fk_archivos_usuarios`
+    FOREIGN KEY (`usuarios_id_usuario`)
+    REFERENCES `macb_archivos`.`usuarios` (`id_usuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -49,13 +59,13 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- User for app
-CREATE USER IF NOT EXISTS 'macb_app'@'localhost'
-IDENTIFIED BY 'MacbApp2026!';
+CREATE USER IF NOT EXISTS 'macb_archivos'@'localhost'
+IDENTIFIED BY 'MacbArchivos2026!';
 
 -- Permisos sobre la base
 GRANT ALL PRIVILEGES
-ON macb_ape.*
-TO 'macb_app'@'localhost';
+ON macb_archivos.*
+TO 'macb_archivos'@'localhost';
 
 FLUSH PRIVILEGES;
 
